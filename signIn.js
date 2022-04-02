53367f7d7e4b3b4b252389398efbb57bd2226412
Tejeswar278 <lsandptlove143@gmail.com>
2022-04-02 18:10:34 +0530

    user credentials corrected

--- signIn.js
+++ signIn.js
@@ -3,33 +3,42 @@ var regUsrs = JSON.parse(localStorage.getItem("DB1")) || [];
 var signInStack = [];
 // console.log(regUsrs);
 function signIn (event){
-    event.preventDefault();
-    var email = document.querySelector("#emailsi").value;
-    // console.log(email);
-    var count = 0;
     
-    for(var i=0; i < regUsrs.length; i++)
-    {
-        if(email==regUsrs[i].emailAdd)
-        {
-            count++;
-            var logindata = {
-                emailAd: email,
-                passwd: regUsrs[i].passwd,
-            };
-            signInStack.push(logindata);
-            localStorage.setItem("signInDataBase", JSON.stringify(signInStack));
-            // alert("email is correct")
-            window.location.href = "login.html"
-            break;
-        }
+    console.log("1")
+     event.preventDefault();
+    console.log("2")
+    var email = document.querySelector("#emailsi").value;
+    console.log("3")
+    localStorage.setItem("signInDataBase", JSON.stringify(email));
+    console.log("4")
+    window.location.href = "login.html";
+    // console.log(email);
+    // var count = 0;
+    
+    // for(var i=0; i < regUsrs.length; i++)
+    // {
+    //     if(email==regUsrs[i].emailAdd)
+    //     {
+    //         count++;
+    //         var logindata = {
+    //             emailAd: email,
+    //             passwd: regUsrs[i].passwd,
+    //         };
+    //         signInStack.push(logindata);
+    //         localStorage.setItem("signInDataBase", JSON.stringify(signInStack));
+    //         // alert("email is correct")
+    //         window.location.href = "login.html"
+    //         break;
+    //     }
         
-    }
-    if(count==0)
-    {
-        alert("Your email is not registered");
-        window.location.href = "signIn.html";
-    }
+    // }
+    // if(count==0)
+    // {
+    //     alert("Your email is not registered");
+    //     
+    // }
+
+    
     
 }
 
